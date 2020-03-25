a698164c39ad9c47f276b5055226c4cbaac4d00a
ahunsader <alex@hunsader.com>
2020-03-25 14:50:01 -0700

    rm hospital_group for now

--- webapp/models.py
+++ webapp/models.py
@@ -3,8 +3,6 @@ from django.db import models
 class Hospital(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     address = models.CharField(max_length=100, blank=False, null=False)
-    # the group of hospital (e.g. VA)
-    hospital_group = models.CharField(max_length=100, blank=False, null=False)
 
 class Supplier(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
