2ee18d1b8af850e68e9a29344d0f41e98ddf7169
ahunsader <alex@hunsader.com>
2020-03-25 14:14:38 -0700

    hospital and supplier schema

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Hospital(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    address = models.CharField(max_length=100, blank=False, null=False)
+    # the group of hospital (e.g. VA)
+    hospital_group = models.CharField(max_length=100, blank=False, null=False)
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    address = models.CharField(max_length=100, blank=False, null=False)
