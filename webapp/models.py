9e647e6d69112039334859e835ec2629072c36ea
ahunsader <alex@hunsader.com>
2020-03-26 14:31:07 -0700

    added to models

--- webapp/models.py
+++ webapp/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
     class UserType(Enum):
         Hospital = 'Hospital'
         Supplier = 'Supplier'
+        HospitalGroup = 'HospitalGroup'
 
     user_type = models.CharField(
         max_length=100,
@@ -17,17 +18,50 @@ class User(AbstractUser):
     )
 
 
+class HospitalGroup(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=False, null=False)
+
+
 class Hospital(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     address = models.CharField(max_length=100, blank=False, null=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=False, null=False)
+    reputation_score = models.FloatField(blank=True, null=True)
+    hospital_group = models.ForeignKey(HospitalGroup, on_delete=models.CASCADE, blank=True, null=True)
+    only_within_group = models.BooleanField(blank=False, null=False, default=False)
 
 
 class Supplier(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     address = models.CharField(max_length=100, blank=False, null=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=False, null=False)
+
+
+class Order(models.Model):
+    num_requested = models.IntegerField(null=False, blank=False)
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE, null=False, blank=False)
+    active = models.BooleanField(null=False, blank=False, default=True)
+    time_submitted = models.DateTimeField(null=False, blank=False, auto_now_add=True)
+    submitter = models.BooleanField(blank=False, null=False)
 
 
 class Ventilator(models.Model):
+    class State(Enum):
+        Available = 'Available'
+        Requested = 'Requested'
+        InTransit = 'InTransit'
+        InUse = 'InUse'
+
     model_num = models.CharField(max_length=128)
+    state = models.CharField(
+        max_length=100,
+        choices=[(tag.value, tag.name) for tag in State],
+        blank=False,
+        null=False,
+        default=State.Available,
+    )
+    owning_hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE, blank=False, null=False, related_name='owning_hospital')
+    current_hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE, blank=False, null=False, related_name='current_hospital')
+    batch_id = models.CharField(max_length=128, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
