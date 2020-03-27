6b66f2ad3378275853d9d6f8c3351d6040a12dd6
Rahul Sheth <56454700+rahulssheth@users.noreply.github.com>
2020-03-27 13:04:57 -0700

    Rahul/email integration (#12)
    Added in email sending through Mailgun and notifications utility file.

--- covid/settings.py
+++ covid/settings.py
@@ -136,10 +136,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'tmp')
-
 LOGIN_REDIRECT_URL = "/home"
 LOGOUT_REDIRECT_URL = "/"
 
 AUTH_USER_MODEL = 'webapp.User'
+
+DEFAULT_EMAIL = 'covid19ventilators@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('MAILGUN_SMTP_SERVER', '')
+EMAIL_PORT = os.environ.get('MAILGUN_SMTP_PORT', '')
+EMAIL_HOST_USER = os.environ.get('MAILGUN_SMTP_LOGIN', '')
+EMAIL_HOST_PASSWORD = os.environ.get('MAILGUN_SMTP_PASSWORD', '')
