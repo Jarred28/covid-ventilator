46b775597093ccef49a54527282c65b71917b84c
Jarred Richards <jarred2828@gmail.com>
2020-04-10 18:13:37 +0200

    add screens for requested and supplied orders

--- static/js/operator/dashboard.js
+++ static/js/operator/dashboard.js
@@ -20,54 +20,50 @@ function getLatlng(address) {
   });
 }
 
-
-function initMap() {
-  var promises = [];
-  for (var i = 0; i < demands.length; i++) {
-    promises.push(getLatlng(demands[i]));
-  }
-
-  for (var i = 0; i < supplies.length; i++) {
-    promises.push(getLatlng(supplies[i]));
-  }
-
-  for (var i = 0; i < transits.length; i++) {
-    promises.push(getLatlng(transits[i]));
-  }
-
-  Promise.all(promises).then(function(res) {
-    locations.demand = res.slice(0, demands.length);
-    locations.supply = res.slice(demands.length, demands.length + supplies.length);
-    locations.transit = res.slice(demands.length + supplies.length, demands.length + supplies.length + transits.length);
-
-    var newyorkBounds = {
-      north: 45.01,
-      south: 40.30,
-      west: -79.46,
-      east: -71.52
-    };
-
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {
-        lat: (newyorkBounds.north + newyorkBounds.south) / 2,
-        lng: (newyorkBounds.west + newyorkBounds.east) / 2
-      },
-      restriction: {
-        latLngBounds: newyorkBounds,
-        strictBounds: false,
-      },
-      zoom: 7
-    });
-
-    for (var type in locations) {
-      locations[type].forEach(function(location) {
-        var marker = new google.maps.Marker({
-          position: location,
-          map: map,
-          icon: '/static/imgs/markers/' + type + '.png',
-        });
-      });
-    }
-  });
+var promises = [];
+for (var i = 0; i < demands.length; i++) {
+  promises.push(getLatlng(demands[i]));
 }
 
+for (var i = 0; i < supplies.length; i++) {
+  promises.push(getLatlng(supplies[i]));
+}
+
+for (var i = 0; i < transits.length; i++) {
+  promises.push(getLatlng(transits[i]));
+}
+
+Promise.all(promises).then(function(res) {
+  locations.demand = res.slice(0, demands.length);
+  locations.supply = res.slice(demands.length, demands.length + supplies.length);
+  locations.transit = res.slice(demands.length + supplies.length, demands.length + supplies.length + transits.length);
+
+  var newyorkBounds = {
+    north: 45.01,
+    south: 40.30,
+    west: -79.46,
+    east: -71.52
+  };
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: (newyorkBounds.north + newyorkBounds.south) / 2,
+      lng: (newyorkBounds.west + newyorkBounds.east) / 2
+    },
+    restriction: {
+      latLngBounds: newyorkBounds,
+      strictBounds: false,
+    },
+    zoom: 7
+  });
+
+  for (var type in locations) {
+    locations[type].forEach(function(location) {
+      var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        icon: '/static/imgs/markers/' + type + '.png',
+      });
+    });
+  }
+});
