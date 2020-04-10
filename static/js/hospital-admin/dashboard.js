ca43e150d5166d06f5a0b342b027da2d509a8c6b
Jarred28 <46113013+Jarred28@users.noreply.github.com>
2020-04-10 13:03:41 -0400

    add screens for requested and supplied orders (#43)

--- static/js/hospital-admin/dashboard.js
+++ static/js/hospital-admin/dashboard.js
@@ -2,13 +2,15 @@ $('#file1').on('change', function () {
   var files = $(this)[0].files;
   if (files.length > 0) {
     $('.fallback').text(files[0].name);
-    // $('#closeBtn').removeClass('d-none');
+    $('#closeBtn').removeClass('d-none');
   } else {
     $('.fallback').text('No file selected');
-    // $('#closeBtn').addClass('d-none');
+    $('#closeBtn').addClass('d-none');
   }
 });
 
 $('body').on('click', '#closeBtn', function() {
-  $('#file1').reset();
+  $('#file1')[0].value = '';
+  $('.fallback').text('No file selected');
+  $(this).addClass('d-none');
 });
