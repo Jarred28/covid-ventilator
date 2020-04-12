be2d73e1f0c1cb2c5188c287c91bcc15acb48185
Avarna Jain <avarnajain@gmail.com>
2020-04-12 12:08:05 -0700

    PUT for ventilators edit

--- webapp/views.py
+++ webapp/views.py
@@ -148,46 +148,63 @@ class VentilatorList(APIView):
         return Response({'ventilators': ventilators, 'serializer': serializer})
 
     def post(self, request, format=None):
+        # Update existing ventilator or add a new one
+        if request.data.get('_method') == "put":
+            if not request.data.get("ventilator-pk", None):
+                return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            if not request.data.get("model_num", None) or not request.data.get("state", None):
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            pk = request.data["ventilator-pk"]
+            model_num = request.data["model_num"]
+            state = request.data["state"]
+            ventilator = Ventilator.objects.get(pk=pk)
+            # Add checks to see if changing status is allowed
+            if ventilator.model_num != model_num:
+                ventilator.model_num = model_num
+            if ventilator.state != state:
+                ventilator.state = state
+            ventilator.save()
+        else:
         # Either batch upload through CSV  or add single ventilator entry
-        csv_file = request.FILES.get('file', None)
-        if csv_file:
-            data_set = csv_file.read().decode('UTF-8')
-            io_string = io.StringIO(data_set)
-            next(io_string)
-            available_vent_ct = Ventilator.objects.filter(owning_hospital=Hospital.objects.get(user=request.user)).filter(Ventilator.State.Available.name).count()
-            src_reserve_ct = Ventilator.objects.filter(owning_hospital=Hospital.objects.get(user=request.user)).filter(state=Ventilator.State.SourceReserve.name).count()
-            vent_ct = available_vent_ct + src_reserve_ct
-            for column in csv.reader(io_string, delimiter=',', quotechar="|"):
-                state = column[1]
-                # We shouldn't be adding another ventilator to the supply unless the ratio is alright.
-                if state == Ventilator.State.Available.name and (src_reserve_ct / (vent_count + 1) < SystemParameters.getInstance().strategic_reserve / 100):
-                    state = Ventilator.State.SourceReserve.name
-                    src_reserve_ct += 1
-                vent_ct += 1
+            csv_file = request.FILES.get('file', None)
+            if csv_file:
+                data_set = csv_file.read().decode('UTF-8')
+                io_string = io.StringIO(data_set)
+                next(io_string)
+                available_vent_ct = Ventilator.objects.filter(owning_hospital=Hospital.objects.get(user=request.user)).filter(Ventilator.State.Available.name).count()
+                src_reserve_ct = Ventilator.objects.filter(owning_hospital=Hospital.objects.get(user=request.user)).filter(state=Ventilator.State.SourceReserve.name).count()
+                vent_ct = available_vent_ct + src_reserve_ct
+                for column in csv.reader(io_string, delimiter=',', quotechar="|"):
+                    state = column[1]
+                    # We shouldn't be adding another ventilator to the supply unless the ratio is alright.
+                    if state == Ventilator.State.Available.name and (src_reserve_ct / (vent_count + 1) < SystemParameters.getInstance().strategic_reserve / 100):
+                        state = Ventilator.State.SourceReserve.name
+                        src_reserve_ct += 1
+                    vent_ct += 1
+                    ventilator = Ventilator(
+                        model_num=column[0], state=state,
+                        owning_hospital=Hospital.objects.get(user=request.user),
+                        current_hospital=Hospital.objects.get(user=request.user)
+                    )
+                    ventilator.save()
+            else:
+                if not request.data.get("model_num", None) or not request.data.get("state", None):
+                    return Response(status=status.HTTP_400_BAD_REQUEST)
+                state = request.data["state"]
+                # If it isn't an available ventilator, it won't mess up supply ratio.
+                if state == Ventilator.State.Available.name:
+                    available_vent_ct = Ventilator.objects.filter(current_hospital=Hospital.objects.get(user=request.user)).filter(state=Ventilator.State.Available.name).count()
+                    src_reserve_ct = Ventilator.objects.filter(current_hospital=Hospital.objects.get(user=request.user)).filter(state=Ventilator.State.SourceReserve.name).count()
+                    vent_ct = available_vent_ct + src_reserve_ct
+                    # If adding this ventilator messes up the strategic reserve ratio, modify it to be held in reserve
+                    if (src_reserve_ct / (vent_ct + 1)) < (SystemParameters.getInstance().strategic_reserve / 100):
+                        state = Ventilator.State.SourceReserve.name
                 ventilator = Ventilator(
-                    model_num=column[0], state=state,
+                    model_num=request.data["model_num"], state=state,
                     owning_hospital=Hospital.objects.get(user=request.user),
                     current_hospital=Hospital.objects.get(user=request.user)
                 )
                 ventilator.save()
-        else:
-            if not request.data.get("model_num", None) or not request.data.get("state", None):
-                return Response(status=status.HTTP_400_BAD_REQUEST)
-            state = request.data["state"]
-            # If it isn't an available ventilator, it won't mess up supply ratio. 
-            if state == Ventilator.State.Available.name:
-                available_vent_ct = Ventilator.objects.filter(current_hospital=Hospital.objects.get(user=request.user)).filter(state=Ventilator.State.Available.name).count()
-                src_reserve_ct = Ventilator.objects.filter(current_hospital=Hospital.objects.get(user=request.user)).filter(state=Ventilator.State.SourceReserve.name).count()
-                vent_ct = available_vent_ct + src_reserve_ct
-                # If adding this ventilator messes up the strategic reserve ratio, modify it to be held in reserve
-                if (src_reserve_ct / (vent_ct + 1)) < (SystemParameters.getInstance().strategic_reserve / 100):
-                    state = Ventilator.State.SourceReserve.name
-            ventilator = Ventilator(
-                model_num=request.data["model_num"], state=state,
-                owning_hospital=Hospital.objects.get(user=request.user),
-                current_hospital=Hospital.objects.get(user=request.user)
-            )
-            ventilator.save()
         ventilators = Ventilator.objects.filter(owning_hospital=Hospital.objects.get(user=request.user))
         serializer = VentilatorSerializer(ventilator)
         return Response({'ventilators': ventilators, 'serializer': serializer})
