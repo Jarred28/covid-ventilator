68a9b48738d11cbd7170036b0fbdb5d55b9fde3b
Jarred <jarred2828@gmail.com>
2020-05-05 11:04:35 +0200

    update permissions

--- webapp/permissions.py
+++ webapp/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework.exceptions import APIException
 from rest_framework.permissions import BasePermission
+from django.db.models import Q
 
 from .models import UserRole
 
@@ -18,15 +19,30 @@ def usertype_permissions(request, user_type):
     elif user_type == 'Supplier':
         roles = UserRole.objects.filter(assigned_user=request.user).filter(supplier__isnull=False)
         isSameRoleLastUsed = lastUsedRole != None and lastUsedRole.supplier != None
-    else:
+    elif user_type == 'System':
         roles = UserRole.objects.filter(assigned_user=request.user).filter(system__isnull=False)
         isSameRoleLastUsed = lastUsedRole != None and lastUsedRole.system != None
+    elif user_type == 'Hospital|Hospital Group':
+        roles = UserRole.objects.filter(assigned_user=request.user).filter(Q(hospital__isnull=False) | Q(hospital_group__isnull=False))
+        isSameRoleLastUsed = lastUsedRole != None and (lastUsedRole.hospital != None or lastUsedRole.hospital_group != None)
 
     if len(roles) == 0:
         raise Forbidden404Exception
     elif not isSameRoleLastUsed:
         UserRole.make_default_role(request.user, roles.first())
-    request.user.set_current_type(user_type)
+
+    currentRole = UserRole.get_default_role(request.user)
+
+    if currentRole.hospital:
+        userType = 'Hospital'
+    elif currentRole.hospital_group:
+        userType = 'Hospital Group'
+    elif currentRole.supplier:
+        userType = 'Supplier'
+    else:
+        userType = 'System'
+
+    request.user.set_current_type(userType)
     return True
 
 class HospitalPermission(BasePermission):
@@ -44,3 +60,7 @@ class SupplierPermission(BasePermission):
 class SystemPermission(BasePermission):
     def has_permission(self, request, view):
         return usertype_permissions(request, 'System')
+
+class HospitalSharedPermission(BasePermission):
+    def has_permission(self, request, view):
+        return usertype_permissions(request, 'Hospital|Hospital Group')
